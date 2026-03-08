747ce5aac49ce6d45f6d19f2cbedf5dad74333ff
scbc-app <lengwetembo@gmail.com>
2026-03-09 01:16:36 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Scbc-Suite/sw.js', { scope: '/Scbc-Suite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
